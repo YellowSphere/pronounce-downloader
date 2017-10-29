210ce9efae6b9d84dc473743005ab701ab631088
Asyanin Mikhail <creative.doomsday@gmail.com>
2017-10-29 23:36:31 +0300

    downloader improvement: donwload one word from user input

--- pronounce.php
+++ pronounce.php
@@ -1,8 +1,10 @@
 <?php
+    $us_pron = "https://dictionary.cambridge.org/media/english/us_pron/";
+    $uk_pron = "https://dictionary.cambridge.org/media/english/uk_pron/";
 
     $customWord = readline("Enter a word: ");
-    $data = file_get_contents('https://dictionary.cambridge.org/dictionary/english/'.$customWord);
-
+    $data = file_get_contents("https://dictionary.cambridge.org/dictionary/english/$customWord");
+    $html = htmlspecialchars($data);
     /*
     example for word kebab:
 
@@ -13,11 +15,14 @@
     https://dictionary.cambridge.org/media/english/uk_pron/u/ukk/ukkaz/ukkazak005.mp3
     */
 
-    $html = htmlspecialchars($data);
-    $pos = strpos($html, "listen to American pronunciation");
+    preg_match('/https:.*.mp3/i', $html, $mp3) or die("Word doesn't exist\n");
+    //echo $mp3[0];
 
-    if ($pos === false) {
-        echo "Entered wrong word: ".$customWord."\n";
-    } else {
-        echo $pos;
+    if (is_dir("sounds") === false) {
+        mkdir("sounds");
     }
+
+    file_put_contents("sounds/$customWord.mp3",
+                fopen("$mp3[0]", 'r'));
+
+    echo("\nDone!\n");
